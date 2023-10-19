bf39f13b159aa3bc0ff02b5eeaede4acfebd7b67
Nimesh <nimesh.palakuttige@sysdata.it>
2023-10-19 14:58:10 +0200

    created ExpenseItem and ExpenseDate components

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,7 +1,24 @@
+// COMPONENT
+import ExpenseDate from '../../components/ExpenseDate/ExpenseDate'
 
-function ExpenseItem(){
+import "./ExpenseItem.css"
+
+
+function ExpenseItem(props){
+
+    // OBJECT DESTRUCTURING
+    const { title, amount, date } = props.expense
+    
     return(
-        <h2>expense item</h2>
+        <div className="expense-item">
+            <div>
+                <ExpenseDate date={date}/>
+            </div>
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${amount}</div>
+            </div>
+        </div>
     )
 }
 
