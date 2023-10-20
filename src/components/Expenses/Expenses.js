8badf7ad52ea34cf71de1c9901a7cd5411de11f9
Nimesh <nimesh.palakuttige@sysdata.it>
2023-10-20 15:57:48 +0200

    added filtr component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import ExpenseItem from "../../components/ExpenseItem/ExpenseItem"
 import ExpensesContainer from "../ExpensesContainer/ExpensesContainer";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 import "./Expenses.css"
 
 function Expenses(props){
+
+    const [yearToFilter, setYearToFilter] = useState('2019')
+
+    function yearChangeHandler(selectedYear){
+        setYearToFilter(selectedYear);
+        console.log(yearToFilter);
+    }
+    
     return(
-        <ExpensesContainer className="expenses">
-            <ExpenseItem expense={props.expenses[0]}/>
-            <ExpenseItem expense={props.expenses[1]}/>
-            <ExpenseItem expense={props.expenses[2]}/>
-            <ExpenseItem expense={props.expenses[3]}/>
-        </ExpensesContainer>
+        <div>
+            <ExpensesContainer className="expenses">
+                <ExpensesFilter defaultYear={yearToFilter} onYearChange={yearChangeHandler} />
+                <ExpenseItem expense={props.expenses[0]}/>
+                <ExpenseItem expense={props.expenses[1]}/>
+                <ExpenseItem expense={props.expenses[2]}/>
+                <ExpenseItem expense={props.expenses[3]}/>
+            </ExpensesContainer>
+        </div>
     )
 }
 
